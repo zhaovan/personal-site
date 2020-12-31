11a4afb37247f197d75d0d97234247eb31d8a18a
Ivan Zhao <ivanzhao8@gmail.com>
2020-12-30 18:38:01 -0800

    image thumbnails

--- next-seo.config.js
+++ next-seo.config.js
@@ -9,7 +9,7 @@ export default {
         site_name: "Ivan's Personal Site",
         images: [
             {
-                url: "https://ivanzhao.me/thumbnail.png",
+                url: "https://ivanzhao.me/thumbnail2.png",
             },
         ],
     },
