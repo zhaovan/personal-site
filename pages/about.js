3d4d636ea5de48898b9a9003683999f04c68904b
Ivan Zhao <ivanzhao8@gmail.com>
2021-11-08 19:39:00 -0500

    fix typo

--- pages/about.js
+++ pages/about.js
@@ -1,23 +1,23 @@
-import NavBar from '../components/navbar';
-import styles from '../styles/About.module.css';
-import shared from '../styles/Shared.module.css';
-import Head from 'next/head';
-import AboutImage from '../public/ivan.png';
-import Footer from '../components/footer';
+import NavBar from "../components/navbar";
+import styles from "../styles/About.module.css";
+import shared from "../styles/Shared.module.css";
+import Head from "next/head";
+import AboutImage from "../public/ivan.png";
+import Footer from "../components/footer";
 
 const links = [
   {
-    text: 'Capitalism, Poverty and Class in Ratatouille',
-    link: 'https://www.youtube.com/watch?v=EC2gRvG1RlI'
+    text: "Capitalism, Poverty and Class in Ratatouille",
+    link: "https://www.youtube.com/watch?v=EC2gRvG1RlI",
   },
   {
-    link: 'https://www.youtube.com/watch?v=4UUJQH7GLms',
-    text: 'Why How to Train Your Dragon has the best opening ever'
+    link: "https://www.youtube.com/watch?v=4UUJQH7GLms",
+    text: "Why How to Train Your Dragon has the best opening ever",
   },
   {
-    link: 'https://placesjournal.org/article/post-it-note-city/',
-    text: 'The Post-It Note City'
-  }
+    link: "https://placesjournal.org/article/post-it-note-city/",
+    text: "The Post-It Note City",
+  },
 ];
 
 export default function About() {
@@ -25,77 +25,89 @@ export default function About() {
     <div className={shared.page}>
       <Head>
         <title>About</title>
-        <link rel='icon' href='/icon.ico' />
+        <link rel="icon" href="/icon.ico" />
       </Head>
-      <NavBar currPage='about' />
+      <NavBar currPage="about" />
       <p className={shared.titleText}>About</p>
       <div>
-        <div className={[shared.container, styles.newContainer].join(' ')}>
-          <div className={styles.photoWrapper} data-aos='fade-right'>
+        <div className={[shared.container, styles.newContainer].join(" ")}>
+          <div className={styles.photoWrapper} data-aos="fade-right">
             <img
               src={AboutImage}
               className={styles.photo}
-              alt='This is a terrible photo of me but with cute cats'
+              alt="This is a terrible photo of me but with cute cats"
             />
           </div>
 
-          <div style={{ marginLeft: '4%' }} >
+          <div style={{ marginLeft: "4%" }}>
             <div>
-              <p className={styles.text} data-aos='fade-left' data-duration="1000">
-                I'm a dog lover from Seattle, WA, trying to fill this world with{' '}
+              <p
+                className={styles.text}
+                data-aos="fade-left"
+                data-duration="1000"
+              >
+                I'm a dog lover from Seattle, WA, trying to fill this world with{" "}
                 <b>
                   baked goods, good vibes, strong drinks, and playful software.
                 </b>
               </p>
-              <p className={styles.text} data-aos='fade-left' data-duration="1000">
+              <p
+                className={styles.text}
+                data-aos="fade-left"
+                data-duration="1000"
+              >
                 Honestly, I'm still trying to figure out what I want to do in
-                life but right now I'm excited about{' '}
-                <a href='https://numinous.productions/ttft' target='_blank'>
+                life but right now I'm excited about{" "}
+                <a href="https://numinous.productions/ttft" target="_blank">
                   tools for thought
                 </a>
-                ,{' '}
-                <a href='https://culdesac.com' target='_blank'>
+                ,{" "}
+                <a href="https://culdesac.com" target="_blank">
                   well designed cities
                 </a>
-                , and{' '}
-                <a href='http://www.celestegame.com/' target='_blank'>
+                , and{" "}
+                <a href="http://www.celestegame.com/" target="_blank">
                   transformative games.
-                </a>{' '}
+                </a>{" "}
               </p>
-              <p className={styles.text} data-aos='fade-left' data-duration="1000">
+              <p
+                className={styles.text}
+                data-aos="fade-left"
+                data-duration="1000"
+              >
                 I fundamentally believe that technology, used judiciously, can
                 build a better future. I spend a lot of time on building
-                ecosystems for better futures{' '}
-                <a href='https://reboothq.substack.com' target='_blank'>
+                ecosystems for better futures{" "}
+                <a href="https://reboothq.substack.com" target="_blank">
                   Reboot
-                </a>{' '}
+                </a>{" "}
                 and{" "}
-                <a href='https://www.dormroomfund.com/' target='_blank'>
+                <a href="https://www.dormroomfund.com/" target="_blank">
                   Dorm Room Fund
                 </a>
-                , and invest in diverse founders at{' '}
-                <a href='https://shl.vc/' target='_blank'>
+                , and invest in diverse founders at{" "}
+                <a href="https://shl.vc/" target="_blank">
                   shl.vc
-                </a>{' '}
-                I've also previouswly worked on improving in home-care for
-                elderly patients at{' '}
-                <a href='https://intuscare.com/' target='_blank'>
+                </a>{" "}
+                I've also previously worked on improving in home-care for
+                elderly patients at{" "}
+                <a href="https://intuscare.com/" target="_blank">
                   Intus Care
                 </a>
-                , giving people the opportunity to live in cities at{' '}
-                <a href='https://www.loftium.com/' target='_blank'>
+                , giving people the opportunity to live in cities at{" "}
+                <a href="https://www.loftium.com/" target="_blank">
                   Loftium
                 </a>
-                , and allowing developers the ability to access location data at{' '}
-                <a href='https://www.askiggy.com/'>Ask Iggy</a>
+                , and allowing developers the ability to access location data at{" "}
+                <a href="https://www.askiggy.com/">Ask Iggy</a>
               </p>
-              <p className={styles.text} data-aos='fade-left' >
+              <p className={styles.text} data-aos="fade-left">
                 <b>Some essays and videos that I love:</b>
                 <ul>
                   {links.map((currLink, i) => {
                     return (
                       <li className={styles.linkItem} key={i}>
-                        <a href={currLink.link} target='_blank'>
+                        <a href={currLink.link} target="_blank">
                           {currLink.text}
                         </a>
                       </li>
