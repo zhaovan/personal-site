9d69659d1014e54c0c485fcc2fa7e1db7c51ce2b
Ivan Zhao <ivanzhao8@gmail.com>
2023-08-16 00:07:16 -0700

    fix seo on pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,10 @@
+"use client";
 import "../styles/globals.css";
 import "../styles/projects.css";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 import AOS from "aos";
 import "aos/dist/aos.css";
-
 import SEO from "../next-seo.config";
 import { DefaultSeo } from "next-seo";
 
@@ -22,6 +22,8 @@ function MyApp({ Component, pageProps }) {
   const pathArray = location.pathname.split("/");
   const currPage = location.query.slug
     ? location.query.slug
+    : pathArray[pathArray.length - 1] === ""
+    ? "home"
     : pathArray[pathArray.length - 1];
 
   const projectOrBlog = pathArray.length > 2;
